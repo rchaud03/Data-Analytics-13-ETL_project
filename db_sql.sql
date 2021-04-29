f91eeea940626dae5e9b8d19746f5bcbab0ada5b
jfhopkins <jfhopkins@hotmail.com>
2021-04-28 21:30:14 -0400

    Movies

--- db_sql.sql
+++ db_sql.sql
@@ -6,3 +6,13 @@ us_voters_votes FLOAT,
 non_us_voters_votes FLOAT,
 average_total_ratings FLOAT
 );
+
+CREATE TABLE movies (
+imdb_title_id varchar(30) PRIMARY KEY,
+title varchar(30),
+year INTEGER,
+genre varchar(30),
+description varchar(30),
+reviews_from_users FLOAT,
+reviews_from_critics FLOAT
+);
